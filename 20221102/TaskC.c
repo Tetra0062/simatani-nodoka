4eee9fa336686a0d674b9415e593d8d68dc94eb3
KUROMARU\kyoma <kyoma.akatuki@gmail.com>
2022-11-09 16:29:06 +0900

    はいれつ

---
+++ 20221102/TaskC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    
+    
+
+    int rand[10]
+
+    rand = data.length;
+    
+    for(i = 0; i <= data.length; ++i){
+        
+        printf("data.length%f",i);
+
+    }
+    
+    return 0;
+
+}
