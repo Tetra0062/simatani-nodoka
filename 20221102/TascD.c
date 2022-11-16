9b9ff437f8398d7394d51fbff309a2abbce0d8f2
KUROMARU\kyoma <kyoma.akatuki@gmail.com>
2022-11-16 16:29:10 +0900

    fizzができない

---
+++ 20221102/TascD.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int main(){
+
+for(int i = 1; i <= 100; i++){
+        
+    //printf("%d\n",i);
+
+    if(i%3==0){
+
+        int Fizz;
+
+        printf("%d\n",Fizz);
+
+    }
+
+     /*   
+    int Fizz;
+    
+        if(i/3==0){
+
+        printf("%d\n",Fizz);
+
+        }
+
+    int Buzz;
+    
+        if(i/5==0){
+
+        printf("%d\n",Buzz);
+
+        }
+
+    int FizzBuzz;
+    
+        if(i/3==0,i/5==0){
+
+        printf("%d\n",FizzBuzz);
+
+        }*/
+
+    }
+
+    return 0;
+
+}
